bdb7a1d394b9ef602dc523ec6340acca1095e4de
root <root@localhost.localdomain>
2021-01-04 16:01:14 +0000

    update code core

--- resources/views/admin/users/default.blade.php
+++ resources/views/admin/users/default.blade.php
@@ -8,50 +8,53 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css">
       <!-- Styles -->
-      <style>
-         html, body {
-         background-color: #fff;
-         color: #636b6f;
-         font-family: 'Nunito', sans-serif;
-         font-weight: 200;
-         height: 100vh;
-         margin: 0;
-         }
-         .full-height {
-         height: 100vh;
-         }
-         .flex-center {
-         align-items: center;
-         display: flex;
-         justify-content: center;
-         }
-         .position-ref {
-         position: relative;
-         }
-         .top-right {
-         position: absolute;
-         right: 10px;
-         top: 18px;
-         }
-         .content {
-         text-align: center;
-         }
-         .title {
-         font-size: 84px;
-         }
-         .links > a {
-         color: #636b6f;
-         padding: 0 25px;
-         font-size: 13px;
-         font-weight: 600;
-         letter-spacing: .1rem;
-         text-decoration: none;
-         text-transform: uppercase;
-         }
-         .m-b-md {
-         margin-bottom: 30px;
-         }
-      </style>
+    <style>
+     html, body {
+     background-color: #fff;
+     color: #636b6f;
+     font-family: 'Nunito', sans-serif;
+     font-weight: 200;
+     height: 100vh;
+     margin: 0;
+     }
+     .full-height {
+     height: 100vh;
+     }
+     .flex-center {
+     align-items: center;
+     display: flex;
+     justify-content: center;
+     }
+     .position-ref {
+     position: relative;
+     }
+     .top-right {
+     position: absolute;
+     right: 10px;
+     top: 18px;
+     }
+     .content {
+     text-align: center;
+     }
+     .title {
+     font-size: 84px;
+     }
+     .links > a {
+     color: #636b6f;
+     padding: 0 25px;
+     font-size: 13px;
+     font-weight: 600;
+     letter-spacing: .1rem;
+     text-decoration: none;
+     text-transform: uppercase;
+     }
+     .m-b-md {
+     margin-bottom: 30px;
+     }
+    </style>
+
+    <script type ="module" src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js"> </script>
+    <script type ="module" src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine-ie11.min.js"> </script>
 
 </head>
 <body>
--- resources/views/livewire/users/user-pagination.blade.php
+++ resources/views/livewire/users/user-pagination.blade.php
@@ -20,4 +20,38 @@
 
     {{ $users->links('vendor.livewire.bootstrap') }}
 
+    <div x-data="{open: false}">
+        <button @click="open=true"> Mở menu thả xuống </button>
+
+        <ul
+            x-show="open"
+             @click.away="open=false"
+         >
+            Nội dung thả xuống
+        </ul>
+    </div>
+
+    <div x-data="{tab: 'foo'}">
+        <button :class="{'active': tab === 'foo'}" @click="tab='foo' "> Foo </button>
+        <button :class="{'active': tab === 'bar'}" @click="tab='bar' "> Thanh </button>
+
+        <div x-show="tab === 'foo' "> Tab Foo </div>
+        <div x-show="tab === 'bar' "> Thanh tab </div>
+    </div>
+
+    <div x-data="{open: false}">
+        <button
+            @mouseenter.once ="
+                fetch('https://jsonplaceholder.typicode.com/todos/1').then
+                    (response => response.json())
+                    .then(json=>{$refs.dropdown.innerHTML=json.title})
+             "
+             @click ="open=true"
+         > Hiển thị menu thả xuống </button>
+
+        <div x-ref="dropdown" x-show ="open" @click.away ="open = false">
+            Đang tải Spinner ...
+        </div>
+    </div>
+
 </div>
