63bed2ae1c5f7b6007f3d3f9c4c415f33b35a675
root <root@centos-7.novalocal>
2021-01-07 07:19:41 +0000

    table demo todo

--- database/migrations/2021_01_04_234159_create_todo_table.php
+++ database/migrations/2021_01_04_234159_create_todo_table.php
@@ -15,6 +15,15 @@ class CreateTodoTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->text('slug')->nullable();
+            $table->text('title')->nullable();
+            $table->text('intro')->nullable();
+            $table->text('detail')->nullable();
+            $table->date('start_date')->nullable();
+            $table->string('image')->nullable();
+            $table->json('showed');
+            $table->boolean('confirmed');
+            $table->unsignedTinyInteger('is_display')->comment('1.public/2.unpublic');
             $table->timestamps();
         });
     }
